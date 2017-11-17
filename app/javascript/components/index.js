cd43135d042769275c4af7b0b3410eafd55586f1
Brandon Patterson <brandnpatterson@gmail.com>
2017-11-16 18:30:05 -0600

    moved components into new folder

--- app/javascript/components/index.js
+++ app/javascript/components/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { render } from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
-import App from './App'
+import App from './components/App'
 
 class Root extends React.Component {
   render () {
